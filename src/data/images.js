5392a137de74e4447e67129ddb0dee0064e5a5a6
Jeremy Bareh <jeremy.gideon@btech.christuniversity.in>
2026-06-26 18:38:41 +0530

    fix: swap hero photo to cinematic cake shot, remove crop constraint

--- src/data/images.js
+++ src/data/images.js
@@ -1,8 +1,8 @@
 const PEXELS = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=800&h=1000&fit=crop`
-const PEXELS_WIDE = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=1000&h=1200&fit=crop`
+const PEXELS_WIDE = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=1200`
 
 export const IMAGES = {
-  hero: PEXELS_WIDE(1028714),
+  hero: PEXELS_WIDE(1721934),
   'vanilla-buttercream': PEXELS(140831),
   cupcakes: PEXELS(14105),
   'chocolate-cake': PEXELS(132694),
